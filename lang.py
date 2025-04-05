909d097243bef3a4ac358292162eee29b7be4356
Chethiya Ravindranath <121000739+ch3thiya@users.noreply.github.com>
2025-04-05 10:09:46 +0530

    Delete lang.py

--- lang.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-api_key = "bb595a55c6ceab254c5518cc73cc288a"
-url = f"https://api.themoviedb.org/3/configuration/languages?api_key={api_key}"
-
-response = requests.get(url)
-languages = response.json()
-
-print(languages)
